a52aa124d31a0bd27403fa13d5d65288097b82d9
Thomas Noone <tnoone@lcs.on.ca>
2024-12-06 15:30:16 -0500

    Added subviews and HongKong + Toronto Time

--- Clock/Views/WorldClockView.swift
+++ Clock/Views/WorldClockView.swift
@@ -10,38 +10,71 @@ import SwiftUI
 struct WorldClockView: View {
     var body: some View {
         NavigationStack{
-            Text("World Clock")
-                .navigationTitle("World Clock")
-                .toolbar {
-                    
-                    ToolbarItem(placement: .topBarLeading) {
+            VStack{
+                
+                //HongKong
+                CountryTime(time: "3:20", hoursDifference: 12, country: "Hong Kong", AMorPM: "AM")
+                //Toronto
+                CountryTime(time: "3:20", hoursDifference: 0, country: "Toronto", AMorPM: "PM")
+                
+                Text("World Clock")
+                    .navigationTitle("World Clock")
+                    .toolbar {
                         
-                        Button("Edit") {
-                            // Does nothing right now
+                        ToolbarItem(placement: .topBarLeading) {
+                            
+                            Button("Edit") {
+                                // Does nothing right now
+                            }
+                            
                         }
                         
-                    }
-                    
-                    ToolbarItem(placement: .primaryAction) {
-                        
-                        Button {
-                            // Does nothing right now
-                        } label: {
-                            Image(systemName: "plus")
+                        ToolbarItem(placement: .primaryAction) {
+                            
+                            Button {
+                                // Does nothing right now
+                            } label: {
+                                Image(systemName: "plus")
+                            }
                         }
+                        
+                        
                     }
-                    
-                    
-                }
+                
+            }
             
         }
         
     }
-    
 }
-
 
 
 #Preview {
     LandingView()
+}
+
+struct CountryTime: View {
+    let time: String
+    let hoursDifference: Int
+    let country: String
+    let AMorPM: String
+    
+    var body: some View {
+        HStack{
+            //Left side
+            VStack{
+                Text("Today, +\(hoursDifference)HRS")
+                Text(country)
+                    .font(.system(.largeTitle, design: .default, weight: .thin))
+            }
+            Spacer()
+            
+            //Right Side
+            Text(time)
+                .font(.system(size: 64.0, weight: .thin, design: .default))
+            Text(AMorPM)
+                .font(.system(.largeTitle, design: .default, weight: .thin))
+            
+        }
+    }
 }
