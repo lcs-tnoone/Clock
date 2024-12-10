566b43c84256e8f014de4000d983d0b1a4bc999d
Thomas Noone <tnoone@lcs.on.ca>
2024-12-10 09:30:51 -0500

    Started stopwatch

--- Clock/Views/StopwatchView.swift
+++ Clock/Views/StopwatchView.swift
@@ -9,10 +9,38 @@ import SwiftUI
 
 struct StopwatchView: View {
     var body: some View {
-        Text("Stopwatch")
+        VStack {
+            Text("00:00:00")
+                .font(.system(size: 60, weight: .bold, design: .monospaced))
+                .padding()
+
+            HStack {
+                Text("00")
+                    .font(.system(size: 40, weight: .bold, design: .monospaced))
+                Text(":")
+                    .font(.system(size: 40, weight: .bold, design: .monospaced))
+                Text("00")
+                    .font(.system(size: 40, weight: .bold, design: .monospaced))
+                Text(":")
+                    .font(.system(size: 40, weight: .bold, design: .monospaced))
+                Text("00")
+                    .font(.system(size: 40, weight: .bold, design: .monospaced))
+            }
+        }
+        .navigationTitle("Stopwatch")
+        .toolbar {
+            ToolbarItem(placement: .primaryAction) {
+                Button {
+                    // Does nothing right now
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+        }
     }
 }
 
+
 #Preview {
-    StopwatchView()
+    LandingView()
 }
